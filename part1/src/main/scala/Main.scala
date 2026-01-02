2e147d66ef996789d5fdc291ec96d77f81da0acf
Anton <anton.haes@vub.be>
2026-01-03 00:12:12 +0100

    model prediction

--- part1/src/main/scala/Main.scala
+++ part1/src/main/scala/Main.scala
@@ -3,6 +3,10 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.feature.VectorIndexer
+import org.apache.spark.ml.regression.{GBTRegressionModel, GBTRegressor}
 
 import DatasetReader._
 
@@ -45,11 +49,27 @@ object Main {
     val staticFeaturesNodeIDDF = staticFeaturesDF
       .withColumn("node", concat(lit("node_"), monotonically_increasing_id() + 1))
       .withColumn("node_id", monotonically_increasing_id() + 1)
+
+    // Convert speed_limit to km/h, that way all measurments are in the same unit => possibly better for ML
+    val staticFeaturesNodeIDSpeedLimitDF = staticFeaturesNodeIDDF.withColumn("speed_limit_kmh",
+      when(col("speed_limit") === 0, lit(Double.NaN))
+        .when(col("speed_limit") === 1, lit(5))
+        .when(col("speed_limit") === 2, lit(20))
+        .when(col("speed_limit") === 3, lit(30))
+        .when(col("speed_limit") === 4, lit(40))
+        .when(col("speed_limit") === 5, lit(50))
+        .when(col("speed_limit") === 6, lit(60))
+        .when(col("speed_limit") === 7, lit(70))
+        .when(col("speed_limit") === 8, lit(80))
+        .when(col("speed_limit") === 9, lit(90))
+        .when(col("speed_limit") === 10, lit(100))
+        .when(col("speed_limit") === 11, lit(110))
+    )
  
     // join the 3 dataframes
     val trafficDF = longSpeedDF
       .join(longVolumeDF, Seq("timestamp", "node"), "full")
-      .join(staticFeaturesNodeIDDF, Seq("node"), "inner")
+      .join(staticFeaturesNodeIDSpeedLimitDF, Seq("node"), "inner")
 
     /* 
      * Task 2: Feature selection
@@ -93,7 +113,7 @@ object Main {
 
     // Convert days of week to ints
     //https://downloads.apache.org/spark/docs/3.5.1/api/scala/org/apache/spark/sql/Column.html#when(condition:org.apache.spark.sql.Column,value:Any):org.apache.spark.sql.Column
-    val finalDF =
+    val trafficWithFeaturesNumericalDF =
       trafficWithFeaturesDF.withColumn(
         "day_of_week",
         when(col("day_of_week") === "Mon", 1)
@@ -106,19 +126,23 @@ object Main {
           .otherwise(-1)
       )
 
+    // add speed in 30 min for data training
+    val finalDF = trafficWithFeaturesNumericalDF
+      .withColumn(
+        "speed_next_30min",
+        lead(col("speed"), 6).over(lagFeatureWindow)
+      )
+
     // Filter columns based on relevance for traffic prediction
     val importantCols = Array(
       "node_id",
       "day_of_week", "week_of_year", "month_of_year", "hour_of_day", "minutes_of_hour",
-      "speed_lag_1","speed_lag_2","speed_lag_3","speed_lag_4","speed_lag_5",
-      "volume_lag_1","volume_lag_2","volume_lag_3","volume_lag_4","volume_lag_5",
+      "speed", "speed_lag_1","speed_lag_2","speed_lag_3","speed_lag_4","speed_lag_5",
+      "volume", "volume_lag_1","volume_lag_2","volume_lag_3","volume_lag_4","volume_lag_5",
       "speed_roll_mean","speed_roll_std",
       "volume_roll_mean","volume_roll_std",
-      "category",
-      "edge_type",
-      "speed_limit",
-      "region_id",
-      "num_segments"
+      "speed_limit_kmh",
+      "speed_next_30min"
     )
 
     // Convert to vector
@@ -126,7 +150,45 @@ object Main {
       .setInputCols(importantCols)
       .setOutputCol("features")
 
-    val processedDF = assembler.transform(finalDF)
+    val processedDF = assembler.transform(finalDF.na.drop())
+
+    /*
+     * Task 3: Using a predictive model
+     */
+    //https://spark.apache.org/docs/latest/ml-classification-regression.html#gradient-boosted-tree-regression
+    // Split data in test and training data
+    val Array(trainingData, testData) = processedDF.randomSplit(Array(0.8, 0.2), seed = 42)
+
+
+    val gbt = new GBTRegressor()
+      .setLabelCol("speed_next_30min")
+      .setFeaturesCol("features")
+      .setMaxIter(50)
+      .setMaxDepth(5) // Default value in Python docs
+
+    val featureIndexer = new VectorIndexer()
+      .setInputCol("features")
+      .setOutputCol("indexedFeatures")
+      .setMaxCategories(2) // we only have continuous data
+      .fit(processedDF)
+
+    val pipeline = new Pipeline()
+      .setStages(Array(featureIndexer, gbt))
+
+    val model = pipeline.fit(trainingData)
+
+    val predictions = model.transform(testData)
+
+    predictions.select("prediction", "speed_next_30min", "features").show(5)
+
+    // Select (prediction, true label) and compute test error.
+    val evaluator = new RegressionEvaluator()
+      .setLabelCol("speed_next_30min")
+      .setPredictionCol("prediction")
+      .setMetricName("rmse")
+    val rmse = evaluator.evaluate(predictions)
+    println(s"Root Mean Squared Error (RMSE) on test data = $rmse")
+
 
 
     spark.stop()
