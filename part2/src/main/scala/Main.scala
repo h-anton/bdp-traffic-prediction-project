3bed9701867332f26c1e273e1ec1844c9b38c504
Anton <anton.haes@vub.be>
2026-01-03 18:16:31 +0100

    incoming stream + join topics

---
+++ part2/src/main/scala/Main.scala
@@ -0,0 +1,90 @@
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.streaming.{StreamingContext, Seconds}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf()
+
+    // If no master is set (e.g., via spark-submit), default to local[*]
+    if (!conf.contains("spark.master")) {
+      conf.setMaster("local[*]")
+    }
+
+    val spark = SparkSession.builder
+      .appName("Project Part 2")
+      .config(conf)
+      .getOrCreate();
+
+    spark.sparkContext.setLogLevel("WARN")
+
+    // kafka config
+    val bootstrapServers = "kafka:9094"
+
+    // Speed and Volume topics return data like this:
+    // {"timestamp":"2024-07-01T19:05:00","node_id":"node_281","value":1}
+    val schema = new StructType()
+      .add("timestamp", StringType)
+      .add("node_id", StringType)
+      .add("value", DoubleType)
+
+    val speedKafkaStream = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", bootstrapServers)
+      .option("subscribe", "speed")
+      .load()
+
+    val volumeKafkaStream = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", bootstrapServers)
+      .option("subscribe", "volume")
+      .load()
+
+    val speedDF = speedKafkaStream
+      .selectExpr("CAST(value AS STRING) as json_str")
+      .select(from_json(col("json_str"), schema).as("data"))
+      .select(
+        col("data.timestamp").cast("timestamp").as("timestamp"),
+        col("data.node_id").as("node"),
+        col("data.value").as("speed")
+      )
+      .withWatermark("timestamp", "5 minutes")
+
+    val volumeDF = volumeKafkaStream
+      .selectExpr("CAST(value AS STRING) as json_str")
+      .select(from_json(col("json_str"), schema).as("data"))
+      .select(
+        col("data.timestamp").cast("timestamp").as("timestamp"),
+        col("data.node_id").as("node"),
+        col("data.value").as("volume")
+      )
+      .withWatermark("timestamp", "5 minutes")
+
+    val joinedDF = speedDF.join(
+      volumeDF,
+      speedDF("node") === volumeDF("node") &&
+      speedDF("timestamp") === volumeDF("timestamp"),
+      "inner"
+    )
+
+    val resultDF = joinedDF.select(
+      speedDF("timestamp"),
+      speedDF("node"),
+      speedDF("speed"),
+      volumeDF("volume")
+    )
+
+    val query = resultDF.writeStream
+      .format("console")
+      .outputMode("append")
+      .option("truncate", false)
+      .start()
+
+    query.awaitTermination()
+
+    //spark.stop()
+  }
+}
